40628b08dc0e6a81115e56b6475e5e6566a16011
Kazbekgit <kazbekgit@gmail.com>
2022-09-22 22:06:21 +0300

    promo

--- source/js/modules/subscription-toggle.js
+++ source/js/modules/subscription-toggle.js
@@ -1,16 +1,31 @@
 const tabs = document.querySelectorAll('.subscription__tabs-item');
+const cards = document.querySelectorAll('.subscription__cards-price');
 
 const toggleTabs = () => {
   tabs.forEach((currentTab) => {
+
     currentTab.addEventListener('click', () => {
-      let tabNumber = currentTab.getAttribute('data-number');
-      let cardSelector = '.' + tabNumber;
 
-      const cards = document.querySelectorAll(cardSelector);
+      if (!currentTab.classList.contains('subscription__tabs-item--active')) {
+        tabs.forEach((elem) => {
+          elem.classList.remove('subscription__tabs-item--active');
+        });
 
-      cards.forEach((card) => {
+        currentTab.classList.add('subscription__tabs-item--active');
 
-      });
+        let tabNumber = currentTab.getAttribute('data-number');
+        let cardSelector = '.' + tabNumber;
+
+        const currentCards = document.querySelectorAll(cardSelector);
+
+        cards.forEach((card) => {
+          card.classList.add('hide');
+        });
+
+        currentCards.forEach((card) => {
+          card.classList.remove('hide');
+        });
+      }
     });
   });
 };
